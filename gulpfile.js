82ebf3728f5d1fd4d94130dff28150ec86d7a586
Ilario Pierbattista <pierbattista.ilario@gmail.com>
2016-03-25 23:12:09 +0100

    Aggiunto sistema di indicizzazione degli articoli

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var fs = require('fs');
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
@@ -17,13 +18,66 @@ gulp.task('babel', function() {
     .pipe(gulp.dest('./js/build'));
 });
 
+
+const INDEXPATH = './html/index.json';
+function addHtml(vynil) {
+    var indexData = readJsonData(INDEXPATH);
+    var identifier = getIdentifier(vynil.relative);
+    indexData.push({
+        "identifier": identifier,
+        "path": vynil.relative
+    });
+    writeJsonData(indexData, INDEXPATH);
+};
+
+function removeHtml(vynil) {
+    var indexData = readJsonData(INDEXPATH);
+    var identifier = getIdentifier(vynil.relative);
+
+    indexData = indexData.filter(function(obj) {
+        return obj.identifier != identifier;
+    })
+    writeJsonData(indexData, INDEXPATH);
+}
+
+function getIdentifier(pathName) {
+    return (pathName.split('.')[0]).replace('/', '_');
+}
+
+function readJsonData(path) {
+    if(fs.existsSync(path)) {
+        return JSON.parse(fs.readFileSync(path));
+    } else {
+        return new Array();
+    }
+}
+
+function writeJsonData(data, path) {
+    fs.writeFile(path, JSON.stringify(data, null, 2), function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    })
+};
+
 gulp.task('watcher', function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
     gulp.watch('./js/src/**/*.js', ['babel']);
+    plugins.watch('./html/**/*.html', {
+        events: ['add']
+    }, addHtml);
+    plugins.watch('./html/**/*.html', {
+        events: ['unlink']
+    }, removeHtml);
 });
 
 gulp.task('bower', function() {
     return plugins.bower({
         directory: './libs'
     });
-})
+});
+
+gulp.task('server', function() {
+    var server = require('./static_server.js');
+    server.create().listen(8888);
+});
