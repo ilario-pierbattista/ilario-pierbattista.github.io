2091b18ad552eb62027fa7e68a82441c102a7660
Ilario Pierbattista <pierbattista.ilario@gmail.com>
2017-04-28 01:13:32 +0200

    Details

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+// $(document).foundation();
+
+$(function () {
+    $("#professional-title").typed({
+        strings: [
+            "Sollevatore di Polemiche",
+            "Lanciatore di Coriandoli",
+            "Lettore di Scatole di Cereali",
+            "Schivatore di Colpi di Scena"
+        ],
+        typeSpeed: 0,
+        loop: true
+    });
+});
