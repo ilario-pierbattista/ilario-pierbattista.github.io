e33cae1d9cc9bab6e26964b2be43c6144b2f9815
Ilario Pierbattista <pierbattista.ilario@gmail.com>
2017-04-28 11:31:01 +0200

    icomoon

--- config.rb
+++ config.rb
@@ -2,7 +2,6 @@
 add_import_path "bower_components/foundation/scss"
 add_import_path "bower_components/font-awesome/scss"
 
-
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
@@ -12,6 +11,7 @@ javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+# output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
---
+++ iconmoon-install.sh
@@ -0,0 +1,18 @@
+argpath=$(realpath $1)
+mkdir .tmp
+cd .tmp
+
+if [ -z $1 ]; then echo "Specify argument";
+else
+    unzip -o $argpath
+    mkdir -p ../scss/icomoon
+    mkdir -p ../fonts
+    cp -Rf fonts/* ../fonts/
+    cp -f style.scss ../scss/icomoon/_icomoon.scss
+    cp -f variables.scss ../scss/icomoon/_variables.scss
+    rm -r *
+    sed -i 's/fonts/..\/fonts/g' ../scss/icomoon/_variables.scss
+fi
+
+cd ..
+rmdir .tmp
